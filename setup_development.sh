365721ab0d166473c3def77d3b45fcb5f2957187
Max Fuxjäger <publicbetamax@gmail.com>
2019-02-18 19:43:33 +0100

    Made setup script easier
    The `--clear` param makes much more sense and makes setups more reliable

--- setup_development.sh
+++ setup_development.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
 sudo apt-get update
 sudo apt-get --assume-yes install python3-pip python3-venv
-if [ ! -d "venv" ]; then
-	python3 -m venv venv
-	source venv/bin/activate
+
+# python3 -m venv --system-site-packages --clear venv
+python3 -m venv --clear venv
+source venv/bin/activate
 	python3 -m pip install --upgrade pip
 	python3 -m pip install --upgrade #MODULENAME
-	deactivate
-fi
+deactivate
